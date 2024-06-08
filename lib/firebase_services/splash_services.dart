9e5db51c86b8ecdbacf3d5905158bea03e0f80ab
hp <hp@gmail.com>
2024-06-08 10:12:56 +0600

    add Feat:
     - check user login or not
     - sign out

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -1,15 +1,24 @@
 import 'dart:async';
 import 'package:all_firebase/ui/auth/login_screen.dart';
+import 'package:all_firebase/ui/posts/posts.dart';
 import 'package:all_firebase/utils/utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class SplashServices {
   void isLogin(context) {
-    Timer(Duration(seconds: 2), () {
-     Utils. navTo(context, LoginScreen());
-    });
+    final auth = FirebaseAuth.instance;
+    // get current user details to check user login or not
+    final user = auth.currentUser;
+    if (user != null) {
+      Timer(Duration(seconds: 2), () {
+        Utils.navTo(context, PostsScreen());
+      });
+    } else {
+      Timer(Duration(seconds: 2), () {
+        Utils.navTo(context, LoginScreen());
+      });
+    }
   }
 }
-
-
--- lib/ui/posts/posts.dart
+++ lib/ui/posts/posts.dart
@@ -1,17 +1,38 @@
+import 'package:all_firebase/ui/auth/login_screen.dart';
+import 'package:all_firebase/utils/utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class PostsScreen extends StatefulWidget {
-  const PostsScreen({ Key? key }) : super(key: key);
+  const PostsScreen({Key? key}) : super(key: key);
 
   @override
   _PostsState createState() => _PostsState();
 }
 
 class _PostsState extends State<PostsScreen> {
+  final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout_rounded),
+            onPressed: () {
+              auth.signOut().then(
+                (value) {
+                  Utils.navTo(context, LoginScreen());
+                },
+              ).onError(
+                (error, stackTrace) {
+                  Utils.toastMessage(error.toString());
+                },
+              );
+            },
+          )
+        ],
+      ),
     );
   }
 }
